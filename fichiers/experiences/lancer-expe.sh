8709145c12556d41b3028ea1239c1401d1b8aadd
Beziarum <paul.beziau@laposte.net>
2017-04-27 18:42:48 +0200

    oui

--- fichiers/experiences/lancer-expe.sh
+++ fichiers/experiences/lancer-expe.sh
@@ -1,11 +1,11 @@
 
 export OMP_NUM_THREADS
 
-ITE=$(seq 10) # nombre de mesures
+ITE=$(seq 2) # nombre de mesures
   
 THREADS=$(seq 2 2 24) # nombre de threads
 
-PARAM="-n -s 4096 -a" # parametres commun à toutes les executions 
+PARAM="-n -s 4096 -a -i 100" # parametres commun à toutes les executions 
 
 execute (){
 EXE="./prog $* $PARAM"
@@ -13,10 +13,9 @@ OUTPUT="$(echo $EXE | tr -d ' ')"
 for nb in $ITE; do for OMP_NUM_THREADS in $THREADS; do  echo -n "$OMP_NUM_THREADS " >> $OUTPUT ; $EXE 2>> $OUTPUT; done; done
 }
 
-execute -v 0
-execute -v 1
-execute -v 2
-execute -v 3
+
+execute -v 5
+execute -v 7
 
 
 
