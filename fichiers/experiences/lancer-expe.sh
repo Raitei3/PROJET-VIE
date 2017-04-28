0052090d099a97df3b1cefff7ae586b922dc4280
Beziarum <paul.beziau@laposte.net>
2017-04-28 11:48:22 +0200

    mise en place du script de lancement

--- fichiers/experiences/lancer-expe.sh
+++ fichiers/experiences/lancer-expe.sh
@@ -5,22 +5,56 @@ ITE=$(seq 2) # nombre de mesures
   
 THREADS=$(seq 2 2 24) # nombre de threads
 
-PARAM="-n -s 4096 -a -i 100" # parametres commun à toutes les executions 
+PARAM="-n" # parametres commun à toutes les executions
+
+ITERATIONS="100
+1000
+10000"
+
+SIZE="256
+1024
+4096"
+
+VERSION=$(seq 7)
+
+SCHEDULE="static
+dynamic"
+
+TILE_SIZE_OMP="32
+64
+128"
+
+TILE_SIZE_OPENCL="16
+32"
 
 execute (){
-EXE="./prog $* $PARAM"
-OUTPUT="$(echo $EXE | tr -d ' ')"
-for nb in $ITE; do
-    for OMP_NUM_THREADS in $THREADS; do
-        echo -n "$OMP_NUM_THREADS " >> $OUTPUT ;
-        $EXE 2>> $OUTPUT;
-    done;
-done
+    for v in $VERSION; do
+        for s in $SIZE; do
+            for ite in $ITERATIONS; do
+                for t in $TILE_SIZE_OMP; do
+                    for OMP_SCHEDULE in $SCHEDULE; do
+                    EXE="./prog $* $PARAM -s $s -i $ite -t $t -v $v"
+                    OUTPUT="$(echo $EXE $OMP_SCHEDULE| tr -d ' ')"
+                    echo $EXE
+                    echo $OMP_SCHEDULE
+                    echo $OUTPUT
+                    for nb in $ITE; do
+                        for OMP_NUM_THREADS in $THREADS; do
+                            echo -n "$OMP_NUM_THREADS " >> $OUTPUT ;
+                            echo $OMP_NUM_THREADS
+                            $EXE 2>> $OUTPUT >/dev/null;
+                        done;
+                    done;
+                    done;
+                done;
+            done
+        done
+    done
 }
 
 
-execute -v 5
-execute -v 7
+execute -a
+execute
 
 
 
